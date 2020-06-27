3fff4727e00b70a530c7904fca38346577d1b849
SirButcher <gabor.butcher@gmail.com>
2020-06-27 19:11:43 +0100

    Add project files.

---
+++ SailingSpeedOMeter.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SailingSpeedOMeter.ino
+ Created:	6/27/2020 7:11:20 PM
+ Author:	SirButcher
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
