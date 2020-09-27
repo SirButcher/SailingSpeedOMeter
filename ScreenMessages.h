11f6f06190b46ddc0bc326cbd2050eb00df776c3
SirButcher <gabor.butcher@gmail.com>
2020-09-27 16:22:57 +0100

    New group, Special Functions added. Fixed the lenght of the example comments.

--- ScreenMessages.h
+++ ScreenMessages.h
@@ -12,28 +12,45 @@ enum ScreenMessage : uint32_t
 	// "   System Ready!    "
 	ScreenMessage_System_SystemReady = 0,
 
+	// "SailingSpeed-O-Meter"
+	ScreenMessage_System_AppName = 20,
+
+	// "      Loading...    "
+	ScreenMessage_System_LoadingCentered = 40,
+
+	// "01234567890123456789"
+	ScreenMessage_System_NumberTest = 60,
+
 	// ---------------------------- Menu messages: ---------------------------
 
 	// Second parameter section
 	// Address range: 8192 - 16383
 
-	// " Just sail          "
+	// "  Just sail         "
 	ScreenMessage_Menu_JustSail = 8192,
 
-	// " Race!              "
+	// "  Race!             "
 	ScreenMessage_Menu_RaceMain = 8212,
 
-	// " Bouy system        "
+	// "  Bouy system       "
 	ScreenMessage_Menu_BouySystem = 8232,
 
-	// " Statistics         "
+	// "  Statistics        "
 	ScreenMessage_Menu_Statistics = 8252,
 
-	// " GPS data           "
+	// "  GPS data          "
 	ScreenMessage_Menu_GPSDataMain = 8272,
 
-	// " Device sleep       "
+	// "  Device sleep      "
 	ScreenMessage_Menu_DeviceSleep = 8292,
+
+	// ------------------------ Special, function keys -----------------------
+
+	// ">                   "
+	ScreenMessage_Spec_HorSelector = 16384,
+
+	// "V                   "
+	ScreenMessage_Spec_VerSelector = 16404,
 
 
 	// ----------------------------  Error messages: -------------------------
